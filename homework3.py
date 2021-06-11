2006edd0f00561c4ab1a5919bb562b7e5763cc0a
unknown <shenpgeng319@gmail.com>
2021-06-11 13:11:25 -0500

    sorting

--- homework3.py
+++ homework3.py
@@ -1,4 +1,15 @@
-# Sorting
+# 1. Find the big number
+# accept input - a list of number '2,5,8,9,18'
+# expected output - max number 18
+"""awn = input("plz enter a list of numbers seperated by commas :       ")
+n1 = awn.split(',')
+mxnumb = 0
+for item in n1:
+    if int(item) > mxnumb:
+        mxnumb = int(item)
+print(mxnumb)"""
+
+# 2. Sorting
 
 # accept input - a list of numbers
 # output - sorted list of numbers
@@ -6,4 +17,18 @@
 # example: 
 # input - 5, 2, 4, 3, 8
 # output - 2, 3, 4, 5, 8
+a = input("plz enter mixed numbers seperated by commas :    ")
+b = a.split(',')
+min1 = 0
+item2 = 0
+result = []
+while len(b) > 0:
+    min1 = b[0]
 
+    for item2 in b:
+        if int(min1) > int(item2):
+            min1 = item2
+
+    result.append(min1)
+    b.remove(min1)
+print(result)
